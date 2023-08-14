b2d4d576319e346aebbf681e0df356210dbabdff
Gerson Sanchez <gsanchez@x-pertec.com>
2023-08-14 15:29:01 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace getrequest
+{
+  class Program {
+    static void Main(string[] args) {
+      string API_URL = "https://jsonplaceholder.typicode.com/posts";
+      var client = new WebClient();
+      var json = client.DownloadString(API_URL);
+      dynamic posts = JsonConvert.DeserializeObject(json);
+
+      foreach(var post in posts) {
+        Console.WriteLine(post.id + ": " + post.title);
+      }
+    }
+  }
+}
